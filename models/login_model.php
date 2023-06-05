c2318ad969f0692881fbc2f7a2fbdee573b30f59
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-06-05 23:44:45 +0530

    remove unwanted comments from the model class

--- models/login_model.php
+++ models/login_model.php
@@ -29,18 +29,5 @@ class LogIn_model{
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
         
         return $result;
-    
-        /*foreach($server_response as $row)
-        {
-            $_SESSION['mail'] = $row['Email'];
-    
-            $_SESSION['first_name'] = $row['First_Name'];
-    
-            $_SESSION['last_name'] = $row['Last_Name'];
-    
-            $_SESSION['id'] = $row['ID'];
-    
-            header("location: home.php");
-        }*/
     }
 }
