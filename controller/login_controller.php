59256354e8c3126069910f8050c85374a66fada2
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-06-06 07:42:13 +0530

    fix undifinded veriable error on auth() function

--- controller/login_controller.php
+++ controller/login_controller.php
@@ -14,70 +14,69 @@ class logIn_Controller
     // that function basically loads the login page
     public function login()
     {
-        $status_data = "";
+        $status_data = "Please LogIn to continue";
 
         require 'views\view_login.php';
-
-        $this->auth();
 
         exit;
     }
 
     // that function basically authenticates the user
+
     public function auth()
     {
-        $password = $_POST['password'];
+        $password = isset($_POST['password']) ? $_POST['password'] : '';
         
-        $email_uname = $_POST['uname'];
-
-        $server_response = $this->login_model->auth($password, $email_uname);
+        $email_uname = isset($_POST['uname']) ? $_POST['uname'] : '';
         
-        if($server_response)
+        if($password == '' || $email_uname == '') 
         {
-            foreach($server_response as $row)
+            $status_data = "Please fill all the fields";
+        
+            require 'views/view_login.php';
+        
+            exit;
+        }else{
+            $server_response = $this->login_model->auth($password, $email_uname);
+        
+            if($server_response) 
             {
-                $_SESSION['mail'] = $row['Email'];
-        
-                $_SESSION['first_name'] = $row['First_Name'];
-        
-                $_SESSION['last_name'] = $row['Last_Name'];
-        
-                $_SESSION['id'] = $row['ID'];
-
-                var_dump($_SESSION['id'], $_SESSION['mail'], $_SESSION['first_name'], $_SESSION['last_name']);
-        
-                require 'views\home.php';
-
+                foreach ($server_response as $row) {
+                    $_SESSION['mail'] = $row['Email'];
+            
+                    $_SESSION['first_name'] = $row['First_Name'];
+            
+                    $_SESSION['last_name'] = $row['Last_Name'];
+            
+                    $_SESSION['id'] = $row['ID'];
+    
+                    var_dump($_SESSION['id'], $_SESSION['mail'], $_SESSION['first_name'], $_SESSION['last_name']);
+            
+                    require 'views/home.php';
+            
+                    exit;
+                }
+            }else 
+            {
+                $status_data = "Wrong username or password";
+            
+                require 'views/view_login.php';
+            
                 exit;
             }
         }
-        else
-        {
-            $status_data =  "Wrong username or password";
-        
-            require 'views\view_login.php';
-
-            exit;
-        }
     }
+}
+
+
+
+
+
+
+
+
+
 
     
-    /*public function logOut()
-        {
-            include 'models\logIn_Model.php';
-    
-            $logIn_Model = new logIn_Model();
-    
-            $logIn_Model->logOut_Model();
-        }
-    
-        public function logIn_View()
-        {
-            include 'views\logIn_View.php';
-        }
-    
-        public function logOut_View()
-        {
-            include 'views\logOut_View.php';
-        }*/
-}
+   
+
