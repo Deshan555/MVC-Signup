68b05cce2d438d5ff27448145810c6627ac3f166
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-06-08 00:04:51 +0530

    import all related css and js files into header and footer

---
+++ assets/includes/main_footer.php
@@ -0,0 +1,8 @@
+  <!-- End your project here-->
+
+  <!-- MDB -->
+  <script type="text/javascript" src="../assets/bootstrap/js/mdb.min.js"></script>
+  <!-- Custom scripts -->
+  <script type="text/javascript"></script>
+
+  </html>
