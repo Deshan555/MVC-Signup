a648dbdc285bb94e709b6b97570735bbb2d29ae3
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-06-08 22:56:58 +0530

    remove imports from home.php and add them to home_header.php file

--- home.php
+++ home.php
@@ -6,6 +6,16 @@ if(!isset($_SESSION['id']))
 {
     header("location: sigin.php");
 }
+else{
+    
+  $first_name = $_SESSION['first_name'];
+
+  $last_name = $_SESSION['last_name'];
+
+  $mail = $_SESSION['mail'];
+
+  $id = $_SESSION['id'];
+}
 
 ?>
 
@@ -23,11 +33,11 @@ if(!isset($_SESSION['id']))
 
 <div class="card">
   
-  <h1><?php echo 'Hello '. $_SESSION['last_name']?></h1>
+  <h1><?php echo 'Hello '. $last_name; ?></h1>
   
-  <p class="title"><?php echo $_SESSION['first_name']?><?php echo' '.$_SESSION['last_name']?></p>
+  <p class="title"><?php echo $first_name; ?><?php echo' '.$$last_name;?></p>
   
-  <p><?php echo $_SESSION['mail']?></p>
+  <p><?php echo $mail;?></p>
   <div style="margin: 24px 0;">
     <a href="#"><i class="fa fa-dribbble"></i></a> 
     <a href="#"><i class="fa fa-twitter"></i></a>  
